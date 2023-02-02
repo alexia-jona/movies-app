23f6a6a4c78ad3db517415345ae01beea83b08de
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2023-02-02 15:08:35 -0600

    added movies on html

--- js/index.js
+++ js/index.js
@@ -1,7 +1,39 @@
 (async () => {
     // This is the entry point for your application. Write all of your code here.
-    // Before you can use the database, you need to configure the "db" object 
+    // Before you can use the database, you need to configure the "db" object
     // with your team name in the "js/movies-api.js" file.
-    
-    
+
+    $.getJSON(`https://api.themoviedb.org/3/search/movie?api_key=${keys.movie_poster_API}&language=en-US&page=1&include_adult=false`).done(function(data)
+    {
+        console.log(data);
+    });
+
+
+    let movies = await getMovies();
+
+    console.log(movies);
+
+    let html = '';
+    for(let movie of movies)
+    {
+
+
+
+
+        html += `<div class="row" id="movie">
+
+                    <div class="column">
+                        <div id="title">${movie.title}</div>
+                        <div id="movie-poster"></div>
+                        <div id="genres">${movie.genre}</div>
+                        <div id="actors">${movie.actors}</div>
+                    </div>
+                    
+                 </div>`
+    }
+
+
+    $('#movies').html(html);
+
+
 })();
--- js/movies-api.js
+++ js/movies-api.js
@@ -45,3 +45,58 @@ const addMovie = async (movie) => {
 
 // Here is where you will create your own functions to further interact with the database.
 // HAPPY CODING!!!
+
+
+
+const getMovie = async (movie) => {
+    try {
+        const url = `/movies/${movie.id}`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        };
+        let response = await db.fetch(url, options);
+        return await response.json();
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+
+
+const updateMovie = async (movie) => {
+    try {
+        const url = `/movies/${movie.id}`;
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movie),
+        };
+        let response = await db.fetch(url, options);
+        return await response.json();
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+
+
+const deleteMovie = async (movie) => {
+    try {
+        const url = `/movies/${movie.id}`;
+        const options = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        };
+        let response = await db.fetch(url, options);
+        return await response.json();
+    } catch (e) {
+        console.error(e);
+    }
+}
